a6d81a627b791f3b0008ba783186d5151ceecfac
irakli qiria <irakliqiria8@gmail.com>
2025-06-23 00:49:58 +0400

    V: 1.0.2

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,24 @@
-export default {
-  input: 'src/index.js',
-  output: {
-    file: 'dist/pure-ui-multiselect.umd.js',
-    format: 'umd',
-    name: 'MultiSelect',
+// rollup.config.js
+import { defineConfig } from 'rollup';
+import path from 'path';
+
+export default defineConfig([
+  // ESM Build
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/pure-ui-multiselect.mjs',
+      format: 'es'
+    }
+  },
+
+  // UMD Build (for CDN <script> usage)
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/pure-ui-multiselect.umd.js',
+      format: 'umd',
+      name: 'MultiSelect'
+    }
   }
-};
+]);
