676b411c0116201fab8a45ba66b0ce37e5dc68b5
irakli qiria <irakliqiria8@gmail.com>
2025-06-23 01:14:41 +0400

    Add CommonJs config in rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -3,12 +3,21 @@ import { defineConfig } from 'rollup';
 import path from 'path';
 
 export default defineConfig([
+  // CommonJS builder (Node.js or older module systems)
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'dist/pure-ui-multiselect.cjs.js',
+      format: 'cjs'
+    }
+  },
+
   // ESM Build
   {
     input: 'src/index.js',
     output: {
-      file: 'dist/pure-ui-multiselect.mjs',
-      format: 'es'
+      file: 'dist/pure-ui-multiselect.esm.js',
+      format: 'esm'
     }
   },
 
